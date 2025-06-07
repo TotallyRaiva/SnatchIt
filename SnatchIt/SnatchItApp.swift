e283f163fd170dbb58454fd1d59ebf1e224be44b
TotallyRaiva <raiva@icloud.com>
2025-06-07 14:52:13 +0300

    Initial commit:Started SnatchIt project

---
+++ SnatchIt/SnatchItApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnatchItApp.swift
+//  SnatchIt
+//
+//  Created by Reiwa on 07.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct SnatchItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
