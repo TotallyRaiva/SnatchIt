37753ca8602366c1279ff8b208886d557d99034d
TotallyRaiva <raiva@icloud.com>
2025-06-08 19:30:54 +0300

    🔥 Integrated Firebase Auth & Firestore. Added basic authentication logic.

---
+++ SnatchIt/AuthService.swift
@@ -0,0 +1,37 @@
+//
+//  AuthService.swift
+//  SnatchIt
+//
+//  Created by Reiwa on 08.06.2025.
+//
+import Foundation
+import FirebaseAuth
+
+class AuthService: ObservableObject{
+    @Published var user: User?
+    
+    func signUp(email: String, password: String, completion: @escaping (Result<Void, Error>) -> Void) {
+        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                self.user = result?.user
+                completion(.success(()))
+            }
+        }
+    }
+    func signIn(email: String, password: String, completion: @escaping (Result<Void, Error>) -> Void) {
+        Auth.auth() .signIn(withEmail: email, password: password) { result, error in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                self.user = result?.user
+                completion(.success(()))
+            }
+        }
+    }
+    func signOut() {
+            try? Auth.auth().signOut()
+            self.user = nil
+    }
+}
