e02226172c7896ecf75059ce60ea9964362f9dd3
TotallyRaiva <raiva@icloud.com>
2025-07-06 15:30:20 +0300

    feat: implement Gang invitation logic (send, accept, decline) in FirestoreService
    - Added sendGangInvite(to:fromGang:) for inviting users
    - Added acceptGangInvite(fromGang:forUser:) for accepting invites
    - Added declineGangInvite(fromGang:forUser:) for declining invites
    - Used Firestore batch writes for atomic operations
    - Prepped backend logic for full invite management flow

--- SnatchIt/FirestoreService.swift
+++ SnatchIt/FirestoreService.swift
@@ -60,3 +60,61 @@ class FirestoreService: ObservableObject {
         }
     }
 }
+
+// MARK: - Gang Invitation Methods
+extension FirestoreService {
+    // Invite user to gang
+    func sendGangInvite(to userId: String, for groupId: String, completion: @escaping (Error?) -> Void) {
+        let groupRef = db.collection("groups").document(groupId)
+        let userRef = db.collection("users").document(userId)
+
+        let batch = db.batch()
+
+        batch.updateData(["pendingInvites": FieldValue.arrayUnion([userId])], forDocument: groupRef)
+        batch.updateData(["gangInvites": FieldValue.arrayUnion([groupId])], forDocument: userRef)
+
+        batch.commit { error in
+            if let error = error {
+                print("Error sending gang invite: \(error)")
+            }
+            completion(error)
+        }
+    }
+
+    // Accept gang invite
+    func acceptGangInvite(groupId: String, userId: String, completion: @escaping (Error?) -> Void) {
+        let groupRef = db.collection("groups").document(groupId)
+        let userRef = db.collection("users").document(userId)
+
+        let batch = db.batch()
+
+        batch.updateData(["pendingInvites": FieldValue.arrayRemove([userId])], forDocument: groupRef)
+        batch.updateData(["members": FieldValue.arrayUnion([userId])], forDocument: groupRef)
+        batch.updateData(["gangInvites": FieldValue.arrayRemove([groupId])], forDocument: userRef)
+
+        batch.commit { error in
+            if let error = error {
+                print("Error accepting gang invite: \(error)")
+            }
+            completion(error)
+        }
+    }
+
+    // Decline gang invite
+    func declineGangInvite(groupId: String, userId: String, completion: @escaping (Error?) -> Void) {
+        let groupRef = db.collection("groups").document(groupId)
+        let userRef = db.collection("users").document(userId)
+
+        let batch = db.batch()
+
+        batch.updateData(["pendingInvites": FieldValue.arrayRemove([userId])], forDocument: groupRef)
+        batch.updateData(["gangInvites": FieldValue.arrayRemove([groupId])], forDocument: userRef)
+
+        batch.commit { error in
+            if let error = error {
+                print("Error declining gang invite: \(error)")
+            }
+            completion(error)
+        }
+    }
+}
